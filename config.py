014182561c03b864c4e422936588ace155d4c60d
wowfilip999 <67916726+wowfilip999@users.noreply.github.com>
2021-04-21 17:20:52 +0200

    forget update config
    comment writed in config working on gui option

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 dom = "yes" # check if end with other as .eu .sk .ca etc...
-wiewthk = "yes"
+resultion = "default" #if wants set custom before edit and write resultion = "custom"
+custom = "" # here write window geometry example 1400x1400
+wiewthk = "yes" # this option for thanks window if write no thans screen not wiew 
+
