faed33eb765dfff20a842f440d79d6c42cb153c1
wowfilip999 <67916726+wowfilip999@users.noreply.github.com>
2021-04-21 17:21:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 dom = "yes" # check if end with other as .eu .sk .ca etc...
 resultion = "default" #if wants set custom before edit and write resultion = "custom"
-custom = "" # here write window geometry example 1400x1400
+custom = "" # here write window geometry example 1400x400
 wiewthk = "yes" # this option for thanks window if write no thans screen not wiew 
 
